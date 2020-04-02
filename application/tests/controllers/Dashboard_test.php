c2426102c55082135b4fe2660e6f3178fd081720
CEDRIC MATASO <langemataso@gmail.com>
2020-04-02 20:56:00 +0200

    change the unitest approach

---
+++ application/tests/controllers/Dashboard_test.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Part of ci-phpunit-test
+ *
+ * @author     Kenji Suzuki <https://github.com/kenjis>
+ * @license    MIT License
+ * @copyright  2015 Kenji Suzuki
+ * @link       https://github.com/kenjis/ci-phpunit-test
+ */
+
+class dahsboard_test extends TestCase
+{
+	public function test_index()
+	{
+		$output = $this->request('GET', 'dashboard');
+		//$this->assertContains('<title>Welcome to CodeIgniter</title>', $output);
+		$this->assertRedirect('auth/login');
+	}
+
+	public function test_method_404()
+	{
+		$this->request('GET', 'dashboard/method_not_exist');
+		$this->assertResponseCode(404);
+	}
+
+	public function test_APPPATH()
+	{
+		$actual = realpath(APPPATH);
+		$expected = realpath(__DIR__ . '/../../');
+		$this->assertEquals(
+			$expected,
+			$actual,
+			'Your APPPATH seems to be wrong. Check your $application_folder in tests/Bootstrap.php'
+		);
+	}
+}
