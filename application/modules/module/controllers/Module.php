8fc23e6cd6f5d8b1035d3aef99ad838fc66648b1
CEDRIC MATASO <langemataso@gmail.com>
2020-06-04 14:14:07 +0200

    ajout des helpers

--- application/modules/module/controllers/Module.php
+++ application/modules/module/controllers/Module.php
@@ -15,8 +15,9 @@ class Module extends MX_Controller {
 		$this->load->helper('path');
 		$this->load->helper('form');
 		$this->load->helper('file');
-		
-		
+        $this->load->helper('unzip_helper');
+        $this->load->helper('mainpage_helper');
+
         $this->load->library('form_validation');
         $this->load->library('ion_auth');
         $this->load->library('ion_auth_acl');
@@ -42,8 +43,53 @@ class Module extends MX_Controller {
             $this->load->view('templates/footer');  
         }
     }
-    public function install(){}
-    public function update(){}
+    public function unzip_module($module_name){
+		$file_name = "./uploads/".$module_name.".zip";
+		$uploads = "./uploads";
+		//echo $uploads;
+		if(unzip($file_name, $uploads, true, false)){
+			//Replace Files
+			full_copy($uploads."/".$module_name, "./application/modules/".$module_name);
+			//echo "Unzip Successfully completed";
+		}else{
+			//echo "Error while unzipping the file";
+		}
+		
+    }
+    public function update_extension($module_name){
+		$module = $this->module_model->get_module_details_by_name($module_name);
+		
+		$parameters = array();
+		$parameters['edd_action'] = 'get_version';
+		$parameters['item_name'] = $module['module_display_name'];
+		$parameters['license'] = $module['license_key'];
+		$parameters['url'] = base_url();
+		
+		$encoded = "";
+		foreach($parameters as $name => $value) {
+			$encoded .= urlencode($name).'='.urlencode($value).'&';
+		}
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, 'http://sanskruti.net/chikitsa/');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST' );
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $encoded);
+		
+
+		$response = curl_exec($ch); 
+		curl_close($ch);    
+		$data = json_decode($response, TRUE);
+		//print_r($data);
+		$download_link = $data['download_link'];
+		
+		$destination = "./uploads/".$module_name.".zip";
+		copy($download_link, $destination);
+		//echo "Module Downloaded";
+		$this->unzip_module($module_name);
+		$this->change_log($module_name);	
+	}
+    
 
 }
 
